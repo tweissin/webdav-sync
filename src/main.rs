8faf32d102111cff6be52b04687ccbcdeeb67d3a
Tom Weissinger <tweissin@gmail.com>
2020-06-13 20:38:08 -0400

    fix up fswatch

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,28 @@
 use notify::{watcher, RecursiveMode, Watcher};
 use std::sync::mpsc::channel;
 use std::time::Duration;
+use notify::DebouncedEvent::{NoticeWrite, NoticeRemove, Create, Write, Remove, Rename};
 
 mod webdav;
 
 fn watch() {
-    // Create a channel to receive the events.
     let (tx, rx) = channel();
-
-    // Create a watcher object, delivering debounced events.
-    // The notification back-end is selected based on the platform.
     let mut watcher = watcher(tx, Duration::from_secs(10)).unwrap();
-
-    // Add a path to be watched. All files and directories at that path and
-    // below will be monitored for changes.
     watcher
         .watch("/Users/tweissin/deletemesoon", RecursiveMode::Recursive)
         .unwrap();
 
     loop {
         match rx.recv() {
-            Ok(event) => println!("{:?}", event),
+            Ok(event) => match event {
+                NoticeWrite(path) => println!("NoticeWrite: {}", path.as_path().display()),
+                NoticeRemove(path) => println!("NoticeRemove: {}", path.as_path().display()),
+                Create(path) => println!("Create: {}", path.as_path().display()),
+                Write(path) => println!("Write: {}", path.as_path().display()),
+                Remove(path) => println!("Remove: {}", path.as_path().display()),
+                Rename(orig, _) => println!("Rename: {}", orig.as_path().display()),
+                _ => (),
+            },
             Err(e) => println!("watch error: {:?}", e),
         }
     }
