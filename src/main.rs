2b00acd5d620ef6e93e3edd37b6c683d591f35f5
Tom Weissinger <tweissin@gmail.com>
2024-11-23 20:17:12 -0500

    use env; get src & dst on rename

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,57 @@
 use clap::Parser;
 mod fswatch;
-use std::io::Result;
+use std::{env, io::Result};
 
 /// Filesystem watcher that syncs with WebDav
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
 struct Args {
     /// WebDav username
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t=get_default_from_env("WEBDAV_USERNAME"))]
     username: String,
 
     /// WebDav password
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t=get_default_from_env("WEBDAV_PASSWORD"))]
     password: String,
 
     /// WebDav hostname
-    #[arg(long)]
+    #[arg(long, default_value_t=get_default_from_env("WEBDAV_HOSTNAME"))]
     hostname: String,
 
     /// Local filesystem directory to watch
-    #[arg(short, long)]
+    #[arg(short, long, default_value_t=get_default_dir())]
     dir: String,
 }
 
 fn main() -> Result<()> {
-    // let args = Args::parse();
-    let args = Args {
-        username: String::from("YOUR_USERNAME"),
-        password: String::from("YOUR_PASSWORD"),
-        hostname: String::from("localhost"),
-        dir: String::from("/tmp")
-    };
+    let args = Args::parse();
 
     let hostname: String = args.hostname;
     let username: String = args.username;
     let password: String = args.password;
     let dir: String = args.dir;
 
-    println!("{}", &hostname);
-    println!("{}", &username);
-    println!("{}", &password);
-    println!("{}", &dir);
+    println!("{}", hostname);
+    println!("{}", username);
+    println!("{}", password);
+    println!("{}", dir);
 
     match fswatch::run(&hostname, &username, &password, &dir) {
-        err => panic!("Problem watching: {:?}", err),
-        _ => Ok(()),
+        Ok(()) => Ok(()),
+        Err(err) => panic!("Problem watching: {:?}", err),
     }
+}
+
+fn get_default_from_env(name: &str) -> String {
+    match env::var(name) {
+        Ok(value) => value,
+        Err(_) => "".to_string(),
+    }
+}
+
+fn get_default_dir() -> String {
+    format!(
+        "{}/deletemesoon",
+        env::var("HOME").unwrap_or_else(|_| "/tmp".to_string())
+    )
 }
